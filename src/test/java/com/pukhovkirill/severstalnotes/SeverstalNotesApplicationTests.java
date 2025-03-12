60d8942f97da55ee9e6e4769f2076c2acf55741f
Pukhov Kirill <kirillpuhov526@gmail.com>
2025-03-12 19:30:04 +0300

    feat: create structure of project, add docker-compose config with postgresql and minio services

---
+++ src/test/java/com/pukhovkirill/severstalnotes/SeverstalNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pukhovkirill.severstalnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverstalNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
