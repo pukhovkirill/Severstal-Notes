ab99f14c6d4a31a4ccf60b816e7fa0e845da9ec5
Pukhov Kirill <kirillpuhov526@gmail.com>
2025-03-13 14:51:42 +0300

    feat: add get note usecase

---
+++ src/main/java/com/pukhovkirill/severstalnotes/usecase/note/GetNoteUseCase.java
@@ -0,0 +1,46 @@
+package com.pukhovkirill.severstalnotes.usecase.note;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import com.pukhovkirill.severstalnotes.entity.model.Note;
+import com.pukhovkirill.severstalnotes.entity.gateway.NoteGateway;
+import com.pukhovkirill.severstalnotes.usecase.UseCase;
+import com.pukhovkirill.severstalnotes.usecase.note.dto.NoteDetails;
+import com.pukhovkirill.severstalnotes.entity.exception.note.NoteNotFoundException;
+
+@RequiredArgsConstructor
+public class GetNoteUseCase implements UseCase<List<Note>, NoteDetails> {
+
+    private final NoteGateway noteGateway;
+
+    @Override
+    public List<Note> execute(NoteDetails... args) {
+        final List<Note> notes;
+
+        notes = new ArrayList<>();
+        if(args.length == 0)
+            return notes;
+
+        Note note;
+
+        int idx = 0;
+        try{
+            for(; idx < args.length; idx++){
+                var nt = args[idx];
+
+                note = noteGateway.findByTitle(nt.getTitle())
+                        .orElseThrow(() -> new NoteNotFoundException(nt.getTitle()));
+                notes.add(note);
+            }
+        }catch(NoteNotFoundException e){
+            var subarray = Arrays.copyOfRange(args, idx+1, args.length);
+            notes.addAll(execute(subarray));
+        }
+
+        return notes;
+    }
+}
